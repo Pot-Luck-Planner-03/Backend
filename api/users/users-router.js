670fb5276caa2fc29c7810bf7b3cae5ff181f167
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-23 11:04:53 -0600

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,7 +5,7 @@ const { restricted } = require("../auth/auth-middleware.js");
 router.get("/", restricted, (req, res, next) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      res.status(200).json(users);
     })
     .catch(next);
 });
@@ -13,9 +13,17 @@ router.get("/", restricted, (req, res, next) => {
 router.get("/:user_id", restricted, (req, res, next) => {
   Users.findById(req.params.user_id)
     .then(user => {
-      res.json(user);
+      res.status(200).json(user);
     })
     .catch(next);
 });
+
+router.put("/:user_id", restricted, (req, res, next) => {
+  Users.editUser(req.params.user_id, req.body)
+    .then(edited => {
+      res.status(200).json(edited)
+    })
+    .catch(next)
+})
 
 module.exports = router;
