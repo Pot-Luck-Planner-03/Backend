7eb6ae8837e6e383cae7f633f9bfb6d0240e69c6
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-24 11:05:45 -0600

    get user potlucks cleaned up

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -21,19 +21,35 @@ function findById(user_id) {
 }
 
 async function getUserPotlucks(user_id) {
+  const users = await find()
   const user = await findById(user_id)
-  const potlucks = db('potlucks AS p')
+  const potlucks = await db('potlucks AS p')
     .join(
       'potluck_users AS pu',
       'pu.potluck_id',
       'p.potluck_id'
       )
     .where('pu.user_id', user_id)
+    .orderBy('p.potluck_date')
 
-console.log("USER", user)
-console.log("POTLUCKS", potlucks)
-
-  return potlucks
+  let userPotlucks = potlucks.map( potluck => {
+    //orgainzer logic would need to change if you change if you are able to delete a user
+      return({
+        attending: potluck.attending,
+        potluck_id: potluck.potluck_id,
+        potluck_name: potluck.potluck_name,
+        organizer: users[potluck.organizer].username,  
+        potluck_description: potluck.potluck_description,
+        potluck_date: potluck.potluck_date,
+        potluck_time: potluck.potluck_time,
+        potluck_location: potluck.potluck_location
+        })
+  })
+  return {
+    user_id: user_id,
+    username: user.username,
+    potlucks: userPotlucks
+  }
 }
 
 async function editUser(user_id, newData) {
